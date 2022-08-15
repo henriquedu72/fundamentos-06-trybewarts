dd9c23b0807c002e8632d8c52bcdc37bd459ddc1
dudure <dudure321@gmail.com>
2022-08-15 02:58:50 -0300

    Algumas correções

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-const agreementCheckBox = document.getElementById('agreement');
-const sendButton = document.getElementById('submit-btn');
 
-function validateFormSubmit() {
-  if (agreementCheckBox.checked === true) {
-    sendButton.disabled = false;
-  } else {
-    sendButton.disabled = true;
-  }
-}
